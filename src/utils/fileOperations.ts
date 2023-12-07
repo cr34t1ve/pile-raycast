38e10938258bddb0118531c7e887c724e9306266
Master Sofua Desmond <mrsofua@gmail.com>
2023-12-07 12:57:31 +0000

    Pull contributions

--- src/utils/fileOperations.ts
+++ src/utils/fileOperations.ts
@@ -1,16 +1,42 @@
 import matter = require("gray-matter");
 import { join } from "path";
-import { writeFile, readFile } from "fs/promises";
-import { PilePost } from "./types";
+import { writeFile, readFile, mkdir } from "fs/promises";
 import { getFilePathForNewPost, getRelativeFilePath } from "../helpers";
-import { showToast } from "@raycast/api";
+import { showToast, LocalStorage } from "@raycast/api";
+import { remark } from "remark";
+import { homedir } from "os";
+import { existsSync } from "fs";
+import { PileDataI } from "./types";
 
 class PileOperations {
-  public static generateMarkdownFile = (content: string, data: any) => {
+  public static generateMarkdownFile = (content: string, data: PileDataI) => {
     return matter.stringify(content, data, { engines: { JSON } });
   };
 
-  public static addFileToIndex = async (path: string, data: PilePost) => {
+  public static generateJSONFileFromMarkdown = (content: string) => {
+    return matter(content);
+  };
+
+  public static generateMarkdownFromHTML = async (html: string) => {
+    return remark().process(html);
+  };
+
+  public static createDirectory = (path: string) => {
+    return new Promise((resolve, reject) => {
+      mkdir(path, { recursive: true })
+        .then(() => {
+          resolve("Directory created successfully");
+        })
+        .catch((error) => {
+          if (error.code === "EEXIST") {
+            resolve("Directory already exists");
+          }
+          reject(error);
+        });
+    });
+  };
+
+  public static addFileToIndex = async (path: string, data: PileDataI) => {
     const filePath = join(path, "index.json");
     const fileData = readFile(filePath, "utf-8");
     const tempData = await fileData;
@@ -32,9 +58,9 @@ class PileOperations {
     });
   };
 
-  public static saveFile = async (path: string, data: string) => {
+  public static saveFile = async (path: string, data: string, isPost: boolean = false) => {
     return new Promise((resolve, reject) => {
-      writeFile(getFilePathForNewPost(path), data, "utf-8")
+      writeFile(isPost ? getFilePathForNewPost(path) : path, data, "utf-8")
         .then(() => {
           resolve("File written successfully");
           showToast({ title: "Created Pile Post", message: "Successfully created Pile Post" });
@@ -43,6 +69,39 @@ class PileOperations {
           reject(error);
         });
     });
+  };
+
+  public static getLocalStorage = async (key: string) => {
+    return await LocalStorage.getItem<string>(key);
+  };
+
+  public static setLocalStorage = async (key: string, value: string) => {
+    return await LocalStorage.setItem(key, value);
+  };
+
+  public static getConfigFilePath = async () => {
+    return join(homedir(), "Piles");
+  };
+
+  public static getConfigFile = async () => {
+    return join(homedir(), "Piles", "piles.json");
+  };
+
+  public static verifyConfigFilePath = async () => {
+    const filePath = join(homedir(), "Piles", "piles.json");
+    const path = join(homedir(), "Piles");
+    if (existsSync(filePath)) {
+      return true;
+    } else {
+      await mkdir(path, { recursive: true });
+      await writeFile(filePath, JSON.stringify([]), "utf-8")
+        .then(() => {
+          return true;
+        })
+        .catch(() => {
+          return false;
+        });
+    }
   };
 }
 
